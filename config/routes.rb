e662607c9e483a961c1d7e972bf53a48b9298d5a
Viktor Ruskai <viktor@ruskai.com>
2023-12-02 23:00:25 +0100

    fix(App): routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
+  root 'static_pages#home'
 
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :videos
+  resources :videos, only: [:index, :show, :create, :destroy]
   resources :video_notes, only: [:create, :edit, :update, :destroy]
 end
