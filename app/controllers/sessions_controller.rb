942c53b1d635dde1a49b7cd998d374bb2e8b3335
Viktor Ruskai <viktor@ruskai.com>
2023-12-23 09:27:33 +0100

    refactor(App): design changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,7 +36,7 @@ class SessionsController < ApplicationController
       end
     else
       flash.now[:danger] = 'Invalid email/password combination.'
-      render 'new', status: :unauthorized
+      render 'new', status: :unprocessable_entity
     end
   end
 
